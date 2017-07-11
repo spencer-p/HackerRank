63959e28bd8077c3375a7c9799163ca2082a6c3f
Spencer Peterson <spencerjpeterson@gmail.com>
2017-07-10 17:32:56 -0700

    sweep algorithm

---
+++ AlgorithmicCrush/main.lua
@@ -0,0 +1,45 @@
+--
+-- Crush
+-- https://www.hackerrank.com/challenges/crush
+--
+-- This is essentially a sweep algorithm problem. How many overlaps do we have?
+-- The twist is that each range adds a different amount to the total, which we 
+-- have to consider. 
+--
+-- What we're going to do is create an array of pairs (position, change).
+-- We'll sort them by position and loop over them, saving the max to print.
+-- So when we get the input:
+--
+-- 1
+-- a b k
+--
+-- The corresponding array would be { { a, k }, { b, -k } }
+--
+
+-- Read input
+-- n (array size) doesn't matter for us
+-- m is the number of ranges we'll be given
+n, m = io.read("*number", "*number", "*l")
+
+positions = {}
+
+for i = 1, m do
+	local a, b, k = io.read("*number", "*number", "*number", "*l")
+	table.insert(positions, { position = a, change = k })
+	table.insert(positions, { position = b, change = -k })
+end
+
+-- Now sort the positions
+table.sort(positions, function(a, b) return a.position < b.position end)
+
+-- Loop through and count the sum, save the max
+max, sum = 0, 0
+for i, p in ipairs(positions) do
+	sum = sum + p.change
+	if sum > max then
+		max = sum
+	end
+end
+
+-- Print the result
+print(max)
